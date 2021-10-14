85679eff12ff892d66b53a17c86ae81112e812bd
lkeleti <keleti.laszlo@vipmail.hu>
2021-10-14 21:51:42 +0200

    week01day04 done

---
+++ day04/daily-solution/src/main/java/week01day04/Hello.java
@@ -0,0 +1,8 @@
+package week01day04;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("week01day04 feladat elkészült.");
+    }
+}
